6a72412e3023fbf220d8d0960d783ab94a972d9b
TApplencourt <t.applencourt@anl.gov>
2019-11-27 18:33:14 +0000

    Fix makefile

--- run.sh
+++ run.sh
@@ -3,8 +3,21 @@
 for dir in $(find omp_tests -type d | sort -r | awk 'a!~"^"$0{a=$0;print}' | sort)
 do
 	echo $dir
-	echo "Testing Compilation"
-	make -C $dir exe 2>&1 | grep make
-	echo "Testing Run"
-	make -C $dir run 2>&1 | grep make
+	make --no-print-directory -C $dir exe  |& tee $dir/exe.log
+	make --no-print-directory -C $dir run  |& tee $dir/run.log 
 done
+
+for dir in $(find omp_tests -type d | sort -r | awk 'a!~"^"$0{a=$0;print}' | sort)
+do
+    echo ""
+    echo "===="
+    echo $dir
+    echo "===="
+    echo "Compilation"
+    echo ""
+    cat $dir/exe.log |  grep make: | sed -r 's/\*{3}//g' |awk '{print substr($0, index($0, $2))}' | column -t | sort | uniq | sort -k2
+    echo "Execution"
+    echo ""
+    cat $dir/run.log |  grep make: | sed -r 's/\*{3}//g' |awk '{print substr($0, index($0, $2))}' | column -t | sort | uniq | sort -k2
+done
+
