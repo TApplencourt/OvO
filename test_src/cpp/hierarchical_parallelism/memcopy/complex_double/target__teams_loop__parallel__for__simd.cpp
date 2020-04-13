3635afb0839f2b6aa73a96b5e24fddb16dc8f2da
TApplencourt <t.applencourt@anl.gov>
2020-04-13 17:38:33 +0000

    Refractor. Add memecopu complex

---
+++ test_src/cpp/hierarchical_parallelism/memcopy/complex_double/target__teams_loop__parallel__for__simd.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <limits>
+
+
+#include <complex>
+using namespace std;
+
+#include <vector>
+#include <algorithm>
+
+bool almost_equal(complex<double> x, complex<double> y, int ulp) {
+
+    bool r = std::fabs(x.real()-y.real()) <= std::numeric_limits<double>::epsilon() * std::fabs(x.real()+y.real()) * ulp ||  std::fabs(x.real()-y.real()) < std::numeric_limits<double>::min();
+    bool i = std::fabs(x.imag()-y.imag()) <= std::numeric_limits<double>::epsilon() * std::fabs(x.imag()+y.imag()) * ulp ||  std::fabs(x.imag()-y.imag()) < std::numeric_limits<double>::min();
+    return r && i;
+
+}
+
+void test_target__teams_loop__parallel__for__simd(){
+  // Input and Outputs
+  
+  const int L = 5;
+  const int M = 6;
+  const int N = 7;
+  const int size = L*M*N;
+  std::vector<complex<double>> A(size);
+  std::vector<complex<double>> B(size);
+  std::generate(B.begin(), B.end(), std::rand);
+
+  complex<double> *pA = A.data();
+  complex<double> *pB = B.data();
+
+// Main program
+
+#pragma omp target   map(from: pA[0:L*M*N]) map(to: pB[0:L*M*N]) 
+
+{
+
+#pragma omp teams loop 
+
+    for (int i = 0 ; i < L ; i++ )
+
+{
+
+#pragma omp parallel 
+
+{
+
+#pragma omp for 
+
+    for (int j = 0 ; j < M ; j++ )
+
+{
+
+#pragma omp simd 
+
+    for (int k = 0 ; k < N ; k++ )
+
+{
+
+
+pA[ k + j*N + i*N*M ] = pB [ k + j*N + i*N*M ];
+
+ }  }  }  }  } 
+
+// Validation
+for (int i = 0 ;  i < size ; i++) {
+    if ( !almost_equal(A[i],B[i],1) ) {
+         std::cerr << "Expected: " << B[i] << " Got: " << A[i] << std::endl;
+        throw std::runtime_error( "target__teams_loop__parallel__for__simd give incorect value when offloaded");
+    }
+}
+ 
+}
+
+int main()
+{
+    test_target__teams_loop__parallel__for__simd();
+}
---
+++ test_src/cpp/hierarchical_parallelism/memcopy/complex_float/target_parallel_loop.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <limits>
+
+
+#include <complex>
+using namespace std;
+
+#include <vector>
+#include <algorithm>
+
+bool almost_equal(complex<float> x, complex<float> y, int ulp) {
+
+    bool r = std::fabs(x.real()-y.real()) <= std::numeric_limits<float>::epsilon() * std::fabs(x.real()+y.real()) * ulp ||  std::fabs(x.real()-y.real()) < std::numeric_limits<float>::min();
+    bool i = std::fabs(x.imag()-y.imag()) <= std::numeric_limits<float>::epsilon() * std::fabs(x.imag()+y.imag()) * ulp ||  std::fabs(x.imag()-y.imag()) < std::numeric_limits<float>::min();
+    return r && i;
+
+}
+
+void test_target_parallel_loop(){
+  // Input and Outputs
+  
+  const int L = 5;
+  const int size = L;
+  std::vector<complex<float>> A(size);
+  std::vector<complex<float>> B(size);
+  std::generate(B.begin(), B.end(), std::rand);
+
+  complex<float> *pA = A.data();
+  complex<float> *pB = B.data();
+
+// Main program
+
+#pragma omp target parallel loop   map(from: pA[0:L]) map(to: pB[0:L]) 
+
+    for (int i = 0 ; i < L ; i++ )
+
+{
+
+
+pA[ i ] = pB [ i ];
+
+ } 
+
+// Validation
+for (int i = 0 ;  i < size ; i++) {
+    if ( !almost_equal(A[i],B[i],1) ) {
+         std::cerr << "Expected: " << B[i] << " Got: " << A[i] << std::endl;
+        throw std::runtime_error( "target_parallel_loop give incorect value when offloaded");
+    }
+}
+ 
+}
+
+int main()
+{
+    test_target_parallel_loop();
+}
