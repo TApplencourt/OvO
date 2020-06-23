cf18ff948bf2f3809bc0dc084881b5e0dfad1cae
TApplencourt <t.applencourt@anl.gov>
2020-06-23 16:00:59 +0000

    Fix memory leak, and improve sh

--- ovo.sh
+++ ovo.sh
@@ -1,40 +1,35 @@
 #!/usr/bin/env bash
 
-# Find path of all directories who contain a Makefile.
-# Those are the test directory
+# Find path of all directories who contain a Makefile, those are the test directory
+# If not argument are passed, we look for the 'test_src'folder
 # The uniq at the end is needed because user can pass the same folder twice in the arguments
-fl_test_src() { find ${@:-test_src} -type f -name 'Makefile' -printf "%h\n" | sort -u ; }
+find_tests_folder() { find ${@:-test_src} -type f -name 'Makefile' -printf "%h\n" | sort -u ; }
+
+fclean() { for dir in $(find_tests_folder $@); do make --silent -C "$dir" clean; done; }
 
 frun() {
     local uuid=$(date +"%Y-%m-%d_%H-%M")
     local result="test_result/${uuid}_$(hostname)"
 
-    for dir in $(fl_test_src $@)
-    do
+    for dir in $(find_tests_folder $@); do
         nresult=$result/${dir#*/}
         echo "Running $dir | Saving log in $nresult"
-
         mkdir -p "$nresult"
-        env > "$nresult"/env.log
-        echo "Trying to get more information about compiler used..." >> "$nresult"/env.log
-        echo "${CXX:-c++} --version" >> "$nresult"/env.log
-        ${CXX:-c++} --version &>> "$nresult"/env.log
-        echo "${FC:-gfortran} --version" >> "$nresult"/env.log
-        ${FC:-gfortran} --version &>> "$nresult"/env.log
+        {
+            env 
+            echo "Trying to get more information about compiler used..."
+            echo "${CXX:-c++} --version"
+            ${CXX:-c++} --version 
+            echo "${FC:-gfortran} --version"
+            ${FC:-gfortran} --version 
+        } &> "$nresult"/env.log
 
-	# Compile in parallel
-	NPROC=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
+	    # Compile in parallel
+	    NPROC=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
         export MAKEFLAGS="${MAKEFLAGS:--j${NPROC:-1} --output-sync}"
         make --no-print-directory -C "$dir" exe |& tee "$nresult"/compilation.log
         # But Run serially
         make -j1 --no-print-directory -C "$dir" run |& tee "$nresult"/runtime.log
-    done
-}
-
-fclean() {
-    for dir in $(fl_test_src $@)
-    do
-        make -s -C "$dir" "clean"
     done
 }
 
@@ -46,28 +41,18 @@ base=$(dirname $0)
 while (( "$#" )); do
     case "$1" in
         gen)
-        shift
-        $base/src/gtest.py "$@"
-        exit
-        ;;
-        run)
-        shift
-        fclean $@ && frun $@
-        exit
-        ;;
-        report)
-        shift
-        $base/src/report.py $@
-        exit
+        shift; $base/src/gtest.py "$@"; exit
         ;;
         clean)
-        fclean
-        exit
+        shift; fclean; exit
         ;;
-        *)
-        shift
+        run)
+        shift; fclean $@ && frun $@; exit
         ;;
-esac
+        report)
+        shift; $base/src/report.py $@; exit
+        ;;
+    esac
 done
 cat $base/src/template/ovo_usage.txt
 exit 0
