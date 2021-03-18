bddc196489260807dd56a87d1ad2e7c35c90e0cc
Thomas Applencourt <tapplencourt@anl.gov>
2021-03-18 20:24:42 +0000

    Reduction_sum -> Reduction_add

---
+++ test_src/cpp/hierarchical_parallelism/reduction_add-complex_double/target_parallel.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <complex>
+using std::complex;
+#ifdef _OPENMP
+#include <omp.h>
+#else
+int omp_get_num_threads() {return 1;}
+#endif
+bool almost_equal(complex<double> x, complex<double> gold, float tol) {
+  if ( (std::signbit(std::real(x)) != std::signbit(std::real(gold))) or (std::signbit(std::imag(x)) != std::signbit(std::imag(gold))) )
+  {
+    x = std::abs(gold) - std::abs(x);
+  }
+  return std::abs(gold) * (1-tol) <= std::abs(x) && std::abs(x) <= std::abs(gold) * (1 + tol);
+}
+void test_target_parallel() {
+  const complex<double> expected_value { 1 };
+  complex<double> counter_parallel{};
+  #pragma omp target parallel reduction(+: counter_parallel)
+  {
+    counter_parallel = counter_parallel + complex<double> { double { 1. } / omp_get_num_threads() };
+  }
+  if (!almost_equal(counter_parallel, expected_value, 0.1)) {
+    std::cerr << "Expected: " << expected_value << " Got: " << counter_parallel << std::endl;
+    std::exit(112);
+  }
+}
+int main()
+{
+    test_target_parallel();
+}
