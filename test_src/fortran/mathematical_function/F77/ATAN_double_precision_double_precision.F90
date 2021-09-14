e3a6ce41444a14323c2cf82aa3340309cd117d3e
Thomas Applencourt <tapplencourt@anl.gov>
2021-09-14 20:42:20 +0000

    Add reciprocal math function

--- test_src/fortran/mathematical_function/F77/ATAN_double_precision_double_precision.F90
+++ test_src/fortran/mathematical_function/F77/ATAN_double_precision_double_precision.F90
@@ -2,12 +2,11 @@ program test_ATAN
    implicit none
    DOUBLE PRECISION :: in0 = ( 0.42 )
    DOUBLE PRECISION :: o_host, o_device
-    o_host = ATAN( in0)
     !$OMP target map(from:o_device)
     o_device = ATAN( in0)
     !$OMP END TARGET
-    IF ( ABS(o_host-o_device) > EPSILON( o_host )*4 ) THEN
-        write(*,*) 'Expected ', o_host, ' Got ', o_device
-        STOP 112
+    IF ( ABS(TAN(o_device) - in0) > EPSILON( in0 )*16 ) THEN
+            write(*,*) 'Expected ', in0, ' Got ', TAN(o_device)
+            STOP 112
     ENDIF
 end program test_ATAN
